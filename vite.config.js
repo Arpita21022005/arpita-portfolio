b0379b844cb7f7ec3d86ef98bfa5cb2fb81353b3
Arpita Lokhande <lokhandearpita21@gmail.com>
2025-08-25 01:31:44 +0530

    Deploy setup complete

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// Update base if your repo name changes
 export default defineConfig({
   plugins: [react()],
-  base: '/arpita-portfolio/',
-})
+  base: "/arpita-portfolio/", // 👈 repo name
+});
